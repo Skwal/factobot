e870cbcc57450789a87834636429c9475dd19ce0
Pascal Peuckert <skwaloo@gmail.com>
2018-03-16 09:58:04 -0400

    Retry when game_id changes

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Bot = require('./src/Bot')
-const FactorioAPI = require('./src/FactorioAPI')
-require('dotenv').config()
 
-const factorioAPI = new FactorioAPI();
-const bot = new Bot(process.env.SLACK_TOKEN, factorioAPI);
+const bot = new Bot()
 bot.start();
 
--- src/Bot.js
+++ src/Bot.js
@@ -1,19 +1,24 @@
-
 const Botkit = require('botkit')
+const FactorioAPI = require('./FactorioAPI')
+require('dotenv').config()
 
 class Bot {
   /**
    * @param {String} token
    * @param {FactorioAPI} factorioAPI
    */
-  constructor(token, factorioAPI) {
+  constructor() {
     this.controller = Botkit.slackbot()
 
-    this.api = factorioAPI
-
     this.bot = this.controller.spawn({
-      token
+      token: process.env.SLACK_TOKEN
     })
+
+    this.init()
+  }
+
+  init() {
+    this.api = new FactorioAPI()
   }
 
   start() {
@@ -36,69 +41,74 @@ class Bot {
       if (err) {
         throw new Error('RTM Close failed unexpectedly!')
       }
-      this.start();
+      this.start()
     });
 
-    this.controller.on('mention', (bot, message) => {
-      if (message.text.indexOf('status') >= 0) {
-        this.bot.reply(message, 'WORKED!')
-      } else {
-        this.bot.reply(message, 'yes?')
-      }
+    this.controller.on('mention', (bot, message) => this.handleMention(message))
+    this.controller.on('ambient', (bot, message) => this.handleAmbient(message))
+  }
+
+  handleMention(message) {
+    this.bot.reply(message, 'Hello!')
+  }
+
+  handleAmbient(message) {
+    if (message.text.indexOf('server status') >= 0) {
+      this.bot.reply(message, 'Let me check...')
+      this.api.getServerStatus().then((server) => this.replyWithSeverStatus(message, server))
+    }
+  }
+
+  replyWithSeverStatus(message, server) {
+    if (!server || !server.game_id) {
+      this.bot.reply(message, "Couldn't find the game. Did the id change? Let's see if I find a new one...")
+      this.init()
+      this.api.getServerStatus().then((server) => this.replyWithSeverStatus(message, server))
+      return
+    }
+
+    const hours = Math.floor(server.game_time_elapsed / 60)
+    const minutes = server.game_time_elapsed % 60
+
+    const time_elapsed = hours + 'h ' + minutes + 'm'
+    const players = server.players
+
+    this.bot.reply(message, {
+      text: "Here it is:",
+      attachments: [
+        {
+          title: 'Game Details',
+          color: '#7CD197',
+          fields: [
+            {
+              title: "Name",
+              value: server.name,
+              short: true
+            },
+            {
+              title: "Version",
+              value: server.application_version.game_version,
+              short: true
+            },
+            {
+              title: "Time Elapsed",
+              value: time_elapsed,
+              short: true
+            },
+            {
+              title: "Description",
+              value: server.description,
+              short: false
+            },
+            {
+              title: "Players Online",
+              value: players && players.length ? players.join(', ') : 'None',
+              short: false
+            }
+          ],
+        }
+      ]
     })
-
-    this.controller.on('ambient', (bot, message) => {
-      if (message.text.indexOf('server status') >= 0) {
-        this.bot.reply(message, 'Let me check...')
-
-        this.api.getServerStatus().then((server) => {
-
-          const hours = Math.floor(server.game_time_elapsed / 60);
-          const minutes = server.game_time_elapsed % 60;
-
-          const time_elapsed = hours + 'h ' + minutes + 'm'
-          const players = server.players
-
-          this.bot.reply(message, {
-            text: "Here it is:",
-            attachments: [
-              {
-                title: 'Game Details',
-                color: '#7CD197',
-                fields: [
-                  {
-                    title: "Name",
-                    value: server.name,
-                    short: true
-                  },
-                  {
-                    title: "Version",
-                    value: server.application_version.game_version,
-                    short: true
-                  },
-                  {
-                    title: "Time Elapsed",
-                    value: time_elapsed,
-                    short: true
-                  },
-                  {
-                    title: "Description",
-                    value: server.description,
-                    short: false
-                  },
-                  {
-                    title: "Players Online",
-                    value: players && players.length ? players.join(', ') : 'None',
-                    short: false
-                  }
-                ],
-              }
-            ]
-          })
-        })
-      }
-    })
-
   }
 }
 
